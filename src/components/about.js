27f529c38351d6a8f3999fd95f807af16c8a157d
jacobhall <hall.j.alexander@gmail.com>
2021-08-12 11:06:08 -0300

    Initial portfolio commit

---
+++ src/components/about.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Fade from "react-reveal/Fade"
+import data from "../yourdata"
+import me from "../images/222473_981532135519_1671070366_n-2.jpg";
+
+const About = () => {
+  return (
+    <div className="secion" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content">
+            <Fade bottom cascade>
+              <h1>About Me</h1>
+            </Fade>
+            <p>
+              {data.aboutParaOne}
+              <br></br>
+              <br></br>
+              {data.aboutParaTwo}
+              <br></br>
+              <br></br>
+              {data.aboutParaThree}
+            </p>
+          </div>
+          <div className="image-wrapper">
+            <img src={me} alt="about"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/yourdata.js
@@ -0,0 +1,198 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+
+  //   Header Details ---------------------
+  name: "Cubby Alexander",
+  headerTagline: [
+    //Line 1 For Header
+    "Full stack w/ focus",
+    //Line 2 For Header
+    "on rapid prototyping",
+    //Line 3 For Header
+    "& modern architecture",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "My specialities are SPAs, mobile apps, and serverless backend architecture. I live for user-centered design and eat, sleep, and breathe quick product iteration cycles.",
+
+  //Contact Email
+  contactEmail: "hall.j.alexander@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Trotr Mobile Application", //Project Title - Add Your Project Title Here
+      para:
+        "Re-engineered an existing feature set for a location sharing app for deployment as a mobile application to both iOS and Android appstores.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Trotr Web Application", //Project Title - Add Your Project Title Here
+      para:
+        "Delivered a social location sharing application complete with authentication, geolaction APIs, ", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Project Five", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      title: "Project Six", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "I'm a full-stack web developer with a background in small business " +
+    "administration and a drive for excellence. The " +
+    "artistry and technical challenge of building digital tools for complex problems " +
+    "inspires me every day.",
+  aboutParaTwo:
+    "I grew up in Hawaii and Maine (nine years each). After studying moral philosophy and political science at " +
+  "the University of King's College in Halifax, I moved to Washington, DC to work in the U.S. Senate. " +
+    "I quickly found entrepreneurship a more compelling focus, and through it came to a deeper understanding and appreciation of technology.",
+  aboutParaThree:
+    "Now I look for opportunities to apply my",
+  aboutImage:
+    "./images/",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "Proficient at developing websites from scratch with HTML 5.",
+    },
+    {
+      img: cssIcon,
+      para:
+        "Able to implement rich styles and designs with CSS 3.",
+    },
+    {
+      img: jsIcon,
+      para:
+        "Competent at implementing complex site features and functionality using ES6 Javascript.",
+    },
+    {
+      img: reactIcon,
+      para:
+        "Highly adept at delivering SPA and mobile solutions coded in React and React Native.",
+    },
+    {
+      img: designIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    {
+      img: codepenIcon,
+      url: "https://www.codepen.com/",
+    },
+    {
+      img: dribbbleIcon,
+      url: "https://dribbble.com/chetanverma",
+    },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
