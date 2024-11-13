a7fc1d09f213db5e984472967b36d0cca3a16a46
Cubby Alexander <hall.j.alexander@gmail.com>
2024-11-13 13:39:15 -0500

    Updates about me, portfolio, and other copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Cubby Alexander Portfolio" />
+    <SEO title="Cubby Hall Portfolio" />
     <Header></Header>
     <Work></Work>
     <About></About>
